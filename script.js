247a7d906a99b1f935be8cb3b051f5f8c12467fb
Maxim Smirnov <45522388+sm1rex@users.noreply.github.com>
2022-06-11 08:22:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(any) {
+    document.querySelector('.starbucks').src = any;
+}
+
+function changeColor(color) {
+    document.querySelector('.circle').style.background = color;
+}
